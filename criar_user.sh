f039331997ff5e713e98aa43d62317885cf2262f
Fercolax <fernando.oab81@outlook.com>
2023-05-22 23:18:10 +0000

    Arquivos IaC v.1

---
+++ criar_user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Criando usuários do sistema...."
+
+useradd guest10 -c "usuario convidado" -s /bin/bash -m
+
+
+useradd guest11 -c "usuario convidado" -s /bin/bash -m
+
+useradd guest12 -c "usuario convidado" -s /bin/bash -m
+
+useradd guest13 -c "usuario convidado" -s /bin/bash -m
+
+echo "Finalizado"
+
